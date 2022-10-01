6273c29dd99cba21adb7df67d10abee2ac84f1f3
Okonkwo Chinonso Emmanuel <milestonetech96@gmail.com>
2022-10-01 13:01:48 +0100

    initial

---
+++ src/types/Job.ts
@@ -0,0 +1,13 @@
+// prettier-ignore
+interface Job {
+    id: number;
+    title: string;
+    location: string;
+    number: number;
+    salary: number;
+}
+
+export default Job;
+
+
+//how to loop through data in Laravel Blade??
---
+++ src/types/OrderTerm.ts
@@ -0,0 +1,5 @@
+type OrderTerm = "location" | "title" | "salary";
+
+export default OrderTerm;
+
+
